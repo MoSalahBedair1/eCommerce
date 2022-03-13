052a9b6df98c6ad71ce3c975a10d0d55c14d3e9d
Muhammad Salah <84187438+MoSalahBedair1@users.noreply.github.com>
2022-03-13 20:35:47 +0200

    dashboard design layout

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,6 +3,54 @@ session_start();
   if (isset($_SESSION['Username'])) {
       $pageTitle = 'Dashboard';
       include 'init.php';
+
+      /* Start Dashboard Page */ ?>
+<div class="home-stats">
+  <div class="container text-center">
+    <h1>Dashboard</h1>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="stat">Total Members<span>100</span></div>
+      </div>
+      <div class="col-md-3">
+        <div class="stat">Pending Members<span>100</span></div>
+      </div>
+      <div class="col-md-3">
+        <div class="stat">Total Items<span>100</span></div>
+      </div>
+      <div class="col-md-3">
+        <div class="stat">Total Comments<span>100</span></div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="latest">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <i class="fa fa-users"></i> Latest Registerd Users
+          </div>
+          <div class="panel-body">Test</div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <i class="fa fa-tag"></i> Latest Items
+          </div>
+          <div class="panel-body">Test</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+
+      /* End Dashboard Page */
+
       include $tpl . 'footer.php';
   } else {
       header('Location: index.php');
