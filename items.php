76b982fb74030841b9e467ec5b77a07c581106ca
Muhammad Salah <84187438+MoSalahBedair1@users.noreply.github.com>
2022-04-01 20:18:08 +0200

    Show item page part 2

--- items.php
+++ items.php
@@ -10,20 +10,48 @@
   $itemid = isset($_GET['itemid']) && is_numeric($_GET['itemid']) ? intval($_GET['itemid']) : 0;
 
   // Select all data depend on this id
-  $stmt = $con->prepare('SELECT * FROM items WHERE Item_ID = ?');
+  $stmt = $con->prepare('SELECT items.*, categories.Name AS category_name, users.Username FROM items INNER JOIN categories ON categories.ID = items.Cat_ID INNER JOIN users ON users.UserID = items.Member_ID WHERE Item_ID = ?');
 
   // Execute query
   $stmt->execute(array($itemid));
 
+  $count = $stmt->rowCount();
+
+  if ($count > 0) {
+
+
   // Fetch the data
-  $item = $stmt->fetch();
+      $item = $stmt->fetch(); ?>
 
-?>
-
-<h1 class="text-center"><?php echo $item['Name'] ?>
+<h1 class="text-center"><?php echo $item['Name']; ?>
 </h1>
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+      <img src="img.png" alt="" class="img-responsive img-thumbnail center-block">
+    </div>
+    <div class="col-md-9">
+      <h2><?php echo $item['Name']; ?>
+      </h2>
+      <p><?php echo $item['Description']; ?>
+      </p>
+      <span><?php echo $item['Add_Date']; ?>
+      </span>
+      <div>Price: <?php echo $item['Price']; ?>
+      </div>
+      <div>Made In: <?php echo $item['Country_Made']; ?>
+      </div>
+      <div>Category: <?php echo $item['category_name']; ?>
+      </div>
+      <div>Added By: <?php echo $item['Username']; ?>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php
-
+  } else {
+      echo 'There\'s no such ID';
+  }
 
   include $tpl . 'footer.php';
