026f8f82e7b01191f6b123c010c088fa4b91d607
Muhammad Salah <84187438+MoSalahBedair1@users.noreply.github.com>
2022-04-05 11:27:25 +0200

    [UPDATE] frontend header

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,44 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo getTitle(); ?>
+  <meta charset="UTF-8" />
+  <title><?php getTitle() ?>
   </title>
-  <link rel="stylesheet" href="<?php echo $css ?>bootstrap.min.css">
   <link rel="stylesheet"
-    href="<?php echo $css ?>font-awesome.min.css">
-  <link rel="stylesheet" href="<?php echo $css ?>frontend.css">
+    href="<?php echo $css ?>bootstrap.min.css" />
+  <link rel="stylesheet"
+    href="<?php echo $css ?>font-awesome.min.css" />
+  <link rel="stylesheet" href="<?php echo $css ?>jquery-ui.css" />
+  <link rel="stylesheet"
+    href="<?php echo $css ?>jquery.selectBoxIt.css" />
+  <link rel="stylesheet" href="<?php echo $css ?>front.css" />
 </head>
 
 <body>
   <div class="upper-bar">
     <div class="container">
       <?php
-        if (isset($_SESSION['user'])) { ?>
+                if (isset($_SESSION['user'])) { ?>
 
-      <img class="my-image img-thumbnail img-circle" src="img.png" alt="">
+      <img class="my-image img-thumbnail img-circle" src="img.png" alt="" />
       <div class="btn-group my-info">
         <span class="btn btn-default dropdown-toggle" data-toggle="dropdown">
           <?php echo $sessionUser ?>
           <span class="caret"></span>
-          <ul class="dropdown-menu">
-            <li><a href="profile.php"></a>My Profile</li>
-            <li><a href="newad.php"></a>New Item</li>
-            <li><a href="profile.php#my-items"></a>My Items</li>
-            <li><a href="logout.php"></a>Logout</li>
-          </ul>
         </span>
+        <ul class="dropdown-menu">
+          <li><a href="profile.php">My Profile</a></li>
+          <li><a href="newad.php">New Item</a></li>
+          <li><a href="profile.php#my-ads">My Items</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
       </div>
 
       <?php
-      } else { ?>
+
+                } else {
+                    ?>
       <a href="login.php">
         <span class="pull-right">Login/Signup</span>
       </a>
-      <?php } ?>
+      <?php
+                } ?>
     </div>
   </div>
   <nav class="navbar navbar-inverse">
@@ -46,20 +51,24 @@
       <div class="navbar-header">
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-nav"
           aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="dashboard.php">Homepage</a>
+        <a class="navbar-brand" href="index.php">Homepage</a>
       </div>
-
       <div class="collapse navbar-collapse" id="app-nav">
         <ul class="nav navbar-nav navbar-right">
           <?php
-          foreach (getCat() as $cat) {
-              echo '<li><a href="categories.php?pageid=' . $cat['ID'] . '&pagename=' . str_replace(' ', '-', $cat['Name']) . '">' . $cat['Name'] . '</a></li>';
-          }
+              $allCats = getAllFrom("*", "categories", "where parent = 0", "", "ID", "ASC");
+            foreach ($allCats as $cat) {
+                echo
+                '<li>
+					<a href="categories.php?pageid=' . $cat['ID'] . '">
+						' . $cat['Name'] . '
+					</a>
+				</li>';
+            }
           ?>
         </ul>
       </div>
