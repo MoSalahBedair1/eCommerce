1a6e9dde5ad35455c472777333997c12dcec28e5
Muhammad Salah <84187438+MoSalahBedair1@users.noreply.github.com>
2022-04-05 11:02:18 +0200

    Update frontend functions file

--- includes/functions/functions.php
+++ includes/functions/functions.php
@@ -1,152 +1,65 @@
 <?php
 
-/*
-** Get categories function v1.0
-** Function to get categories from database
-*/
+    /*
+    ** Get All Function v2.0
+    ** Function To Get All Records From Any Database Table
+    */
 
-function getCat()
-{
-    global $con;
-    $getCat = $con->prepare("SELECT * FROM categories ORDER BY ID ASC");
-    $getCat->execute();
-    $cats = $getCat->fetchAll();
-    return $cats;
-}
+    function getAllFrom($field, $table, $where = null, $and = null, $orderfield, $ordering = "DESC")
+    {
+        global $con;
 
-/*
-** Get Ad items function v1.0
-** Function to get Ad items from database
-*/
+        $getAll = $con->prepare("SELECT $field FROM $table $where $and ORDER BY $orderfield $ordering");
 
-function getItems($where, $value, $approve = null)
-{
-    global $con;
+        $getAll->execute();
 
-    if ($approve == null) {
-        $sql = 'AND Approve = 1';
-    } else {
-        $sql = null;
+        $all = $getAll->fetchAll();
+
+        return $all;
+    }
+    
+    /*
+    ** Check If User Is Not Activated
+    ** Function To Check The RegStatus Of The User
+    */
+
+    function checkUserStatus($user)
+    {
+        global $con;
+
+        $stmtx = $con->prepare("SELECT 
+									Username, RegStatus 
+								FROM 
+									users 
+								WHERE 
+									Username = ? 
+								AND 
+									RegStatus = 0");
+
+        $stmtx->execute(array($user));
+
+        $status = $stmtx->rowCount();
+
+        return $status;
     }
 
-    $getItems = $con->prepare("SELECT * FROM items WHERE $where = ? $sql ORDER BY Item_ID DESC");
-    $getItems->execute(array($value));
-    $items = $getItems->fetchAll();
-    return $items;
-}
+    /*
+    ** Check Items Function v1.0
+    ** Function to Check Item In Database [ Function Accept Parameters ]
+    ** $select = The Item To Select [ Example: user, item, category ]
+    ** $from = The Table To Select From [ Example: users, items, categories ]
+    ** $value = The Value Of Select [ Example: Osama, Box, Electronics ]
+    */
 
-/*
-** Check if user is not activated
-** Function to chec the RegStatus of the user
-*/
+    function checkItem($select, $from, $value)
+    {
+        global $con;
 
-function checkUserStatus($user)
-{
-    // Check if the user exists in database
+        $statement = $con->prepare("SELECT $select FROM $from WHERE $select = ?");
 
-    global $con;
+        $statement->execute(array($value));
 
-    $stmtx = $con->prepare('SELECT Username, RegStatus FROM users WHERE Username = ? AND RegStatus = 0');
-    $stmtx->execute(array($user));
-    $status = $stmtx->rowCount();
-    return $status;
-}
+        $count = $statement->rowCount();
 
-
-/*
- ** Title function v1.0
- ** Title function that echo the page title in case the page
- **  has the variable $pageTitle and echo default title for other pages
- */
-
-
-  function getTitle()
-  {
-      global $pageTitle;
-      if (isset($pageTitle)) {
-          echo $pageTitle;
-      } else {
-          echo 'Default';
-      }
-  }
-
-
-/*
- ** Redirect function v2.0
- ** This function accepts parameters
- ** $theMsg = Echo the message [ error | success | warning ]
- ** $url = The link you want to redirect to
- ** $seconds = seconds before redirecting
- */
-
-  function redirect($theMsg, $url = null, $seconds = 3)
-  {
-      if ($url === null) {
-          $url = 'index.php';
-      } else {
-          if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] !== '') {
-              $url = $_SERVER['HTTP_REFERER'];
-              $link = 'Previous page';
-          } else {
-              $url = 'index.php';
-              $link = 'Homepage';
-          }
-      }
-
-      echo $theMsg;
-      echo "<div class='alert alert-info'>You will be redirected to $link after $seconds  seconds.</div>";
-      header("refresh:$seconds;url=$url");
-      exit();
-  }
-
-/*
-** Check items function v1.0
-** Function to check item in database [ function accept parameters ]
-** $select = The item to select [ Example: user, item, category ]
-** $from = The table to select from [ Example: users, items, categories ]
-** $value = The value of select [ Example: Osama, Box, Electronics ]
-*/
-
-function checkItem($select, $from, $value)
-{
-    global $con;
-
-    $statement = $con->prepare("SELECT $select FROM $from WHERE $select = ?");
-    $statement->execute(array($value));
-    $count = $statement->rowCount();
-
-    return $count;
-}
-
-/*
-** Count number of items function v1.0
-** function to count number of items rows
-** $item = the item to count
-** $table = the table to choose from
-*/
-
-function countItems($item, $table)
-{
-    global $con;
-    $stmt2 = $con->prepare("SELECT COUNT($item) FROM $table");
-    $stmt2->execute();
-    return $stmt2->fetchColumn();
-}
-
-/*
-** Get latest records function v1.0
-** Function to get latest items from database [Users, Items, Comments]
-** $select = Field To Select
-** $table = The table to choose from
-** $order = the desc ordering
-** $limit = Number of records to get
-*/
-
-function getLatest($select, $table, $order, $limit = 5)
-{
-    global $con;
-    $getStmt = $con->prepare("SELECT $select FROM $table ORDER BY $order DESC LIMIT $limit");
-    $getStmt->execute();
-    $rows = $getStmt->fetchAll();
-    return $rows;
-}
+        return $count;
+    }
