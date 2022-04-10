201b4d252181ee013ad293aca0f3683f2fcd9c63
Muhammad Salah <84187438+MoSalahBedair1@users.noreply.github.com>
2022-04-10 16:58:12 +0200

    fix

--- init.php
+++ init.php
@@ -1,31 +1,28 @@
 <?php
 
-	// Error Reporting
+    // Error Reporting
 
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
 
-	include 'admin/connect.php';
+    include 'admin/connect.php';
 
-	$sessionUser = '';
-	
-	if (isset($_SESSION['user'])) {
-		$sessionUser = $_SESSION['user'];
-	}
+    $sessionUser = '';
+    
+    if (isset($_SESSION['user'])) {
+        $sessionUser = $_SESSION['user'];
+    }
 
-	// Routes
+    // Routes
 
-	$tpl 	= 'includes/templates/'; // Template Directory
-	$lang 	= 'includes/languages/'; // Language Directory
-	$func	= 'includes/functions/'; // Functions Directory
-	$css 	= 'layout/css/'; // Css Directory
-	$js 	= 'layout/js/'; // Js Directory
+    $tpl 	= 'includes/templates/'; // Template Directory
+    $lang 	= 'includes/languages/'; // Language Directory
+    $func	= 'includes/functions/'; // Functions Directory
+    $css 	= 'layout/css/'; // Css Directory
+    $js 	= 'layout/js/'; // Js Directory
 
-	// Include The Important Files
+    // Include The Important Files
 
-	include $func . 'functions.php';
-	include $lang . 'english.php';
-	include $tpl . 'header.php';
-	
-
-	
+    include $func . 'functions.php';
+    include $lang . 'english.php';
+    include $tpl . 'header.php';
